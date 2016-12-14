89d453d37af2627e3e447857b0520d2934fe96fe
flycorn <ym1992it@gmail.com>
2016-12-14 13:15:47 +0800

    更新error模板

--- app/Providers/ControllerProvider.php
+++ app/Providers/ControllerProvider.php
@@ -10,14 +10,9 @@ use Phalcon\Mvc\Controller;
 abstract class ControllerProvider extends Controller
 {
     //404错误
-    public function error404Action()
-    {
-        echo '404错误';
-    }
+    public function error404Action(){}
 
     //500错误
-    public function error500Action()
-    {
-        echo '500错误';
-    }
+    public function error500Action(){}
+
 }
